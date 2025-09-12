b17932ce03a7088b8206c23ec45e105f1eb8358a
Shaddycracker <ladydon986@gmail.com>
2025-09-12 12:37:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,36 @@
 //
-// import Hero from "@/components/hero"
-// import Navbar from "@/components/navbar"
-// import NoticeSection from "@/components/notice-section"
-// import ImageCarousel from "@/components/image-carousel"
-// import InquiryAchievers from "@/components/inquiry-achievers"
-// import NoticeAchiver from "@/components/notice-achivers"
-// import Footer from "@/components/footer"
-// import Faculties from "@/components/faculty-section";
-// import {ThoughtMarquee} from "@/components/ThoughtMarquee";
-// import DataItems from "@/components/Data-Item";
-import Maintain from "@/components/maintain"
+import Hero from "@/components/hero"
+import Navbar from "@/components/navbar"
+import NoticeSection from "@/components/notice-section"
+import ImageCarousel from "@/components/image-carousel"
+import InquiryAchievers from "@/components/inquiry-achievers"
+import NoticeAchiver from "@/components/notice-achivers"
+import Footer from "@/components/footer"
+import Faculties from "@/components/faculty-section";
+import {ThoughtMarquee} from "@/components/ThoughtMarquee";
+import DataItems from "@/components/Data-Item";
+
 export default function Home() {
-    return <Maintain/>
-  // return (
-  //   <div className="min-h-screen bg-background">
-  //     <Navbar />
-  //       <main>
-  //           <ImageCarousel/>
-  //
-  //               <ThoughtMarquee
-  //                   text="The only way to do great work is to love what you do. - Steve Jobs"
-  //                   direction="left"
-  //                   className="bg-primary/10"
-  //               />
-  //               <Hero/>
-  //               <NoticeAchiver/>
-  //               <DataItems/>
-  //               <Faculties/>
-  //
-  //               <InquiryAchievers/>
-  //
-  //       </main>
-  //       <Footer/>
-  //   </div>
-  // )
+   return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+        <main>
+            <ImageCarousel/>
+  
+                <ThoughtMarquee
+                    text="The only way to do great work is to love what you do. - Steve Jobs"
+                    direction="left"
+                    className="bg-primary/10"
+                />
+                <Hero/>
+                <NoticeAchiver/>
+                <DataItems/>
+                <Faculties/>
+  
+                <InquiryAchievers/>
+  
+        </main>
+        <Footer/>
+    </div>
+  )
 }
