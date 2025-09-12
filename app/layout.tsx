4ef9e19768fd347fa632bed5c58726d3f9846079
Shaddycracker <ladydon986@gmail.com>
2025-09-12 13:11:32 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,25 @@ import { EdgeStoreProvider } from '@/lib/edgestore';
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "EMRS",
-  description: "A premier educational institution dedicated to excellence",
-    generator: 'v0.dev'
-}
+  title: 'EMRS Bahraich',
+  description: 'Official website of EMRS Bahraich school',
+  openGraph: {
+    title: 'EMRS Bahraich',
+    description: 'Official website of EMRS Bahraich school',
+    url: 'https://www.emrsbahraich.in',
+    siteName: 'EMRS Bahraich',
+    images: [
+      {
+        url: 'https://www.emrsbahraich.in/og-image.jpg', // <-- create an image
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_IN',
+    type: 'website',
+  },
+};
+
 
 export default function RootLayout({
   children,
