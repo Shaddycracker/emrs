0dc889525d2ef142b15b827add55af9c606ebfff
infonextgenscale-coder <infonextgenscale@gmail.com>
2025-09-21 00:55:59 +0530

    Update page.tsx

--- app/gallery-photo-video/page.tsx
+++ app/gallery-photo-video/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { getAllGalleryVideos } from "@/firebase/Video-Gallery/Gallery"; // Assuming this path
